1241afe7a09a4fc479321131436d6bef0f551454
Luidgi <camila.sajnin@mprj.mp.br>
2026-01-06 10:31:36 -0300

    Fix connection with db

--- backend/config.py
+++ backend/config.py
@@ -4,12 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5432')
-    DB_NAME = os.getenv('DB_NAME', 'dudu_cookies')
-    DB_USER = os.getenv('DB_USER', 'postgres')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
-    
     @property
     def database_url(self):
-        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return os.getenv('DB_URL')
